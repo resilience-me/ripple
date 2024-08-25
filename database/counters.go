dc56b3eafcfb48976c73de1ac922c8e70d282b29
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 10:52:11 +0200

    Update counters.go

--- database/counters.go
+++ database/counters.go
@@ -1,40 +1,36 @@
 package database
 
-import (
-	"ripple/types"
-)
-
-// GetCounter retrieves the counter value using the datagram to determine the directory.
-func GetCounter(dg *types.Datagram) (uint32, error) {
-	accountDir := GetAccountDir(dg.Username)
+// GetCounter retrieves the counter value using the username to determine the directory.
+func GetCounter(username string) (uint32, error) {
+	accountDir := GetAccountDir(username)
 	return GetUint32FromFile(accountDir, "counter.txt")
 }
 
-// SetCounter sets the counter value.
-func SetCounter(dg *types.Datagram) error {
-	accountDir := GetAccountDir(dg.Username)
-	return WriteUint32ToFile(accountDir, "counter.txt", dg.Counter)
+// SetCounter sets the counter value using the username to determine the directory.
+func SetCounter(username string, value uint32) error {
+	accountDir := GetAccountDir(username)
+	return WriteUint32ToFile(accountDir, "counter.txt", value)
 }
 
-// GetCounterIn retrieves the counter_in value using the datagram to determine the directory.
-func GetCounterIn(dg *types.Datagram) (uint32, error) {
-	peerDir := GetPeerDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
+// GetCounterIn retrieves the counter_in value using the user and peer identifiers to determine the directory.
+func GetCounterIn(username, peerServerAddress, peerUsername string) (uint32, error) {
+	peerDir := GetPeerDir(username, peerServerAddress, peerUsername)
 	return GetUint32FromFile(peerDir, "counter_in.txt")
 }
 
-// SetCounterIn sets the counter_in value.
-func SetCounterIn(dg *types.Datagram) error {
-	peerDir := GetPeerDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
-	return WriteUint32ToFile(peerDir, "counter_in.txt", dg.Counter)
+// SetCounterIn sets the counter_in value using the user and peer identifiers to determine the directory.
+func SetCounterIn(username, peerServerAddress, peerUsername string, value uint32) error {
+	peerDir := GetPeerDir(username, peerServerAddress, peerUsername)
+	return WriteUint32ToFile(peerDir, "counter_in.txt", value)
 }
 
-// GetCounterOut retrieves the counter_out value using the datagram to determine the directory.
+// GetCounterOut retrieves the counter_out value using the user and peer identifiers to determine the directory.
 func GetCounterOut(username, peerServerAddress, peerUsername string) (uint32, error) {
 	peerDir := GetPeerDir(username, peerServerAddress, peerUsername)
 	return GetUint32FromFile(peerDir, "counter_out.txt")
 }
 
-// SetCounterOut sets the counter_out value.
+// SetCounterOut sets the counter_out value using the user and peer identifiers to determine the directory.
 func SetCounterOut(username, peerServerAddress, peerUsername string, value uint32) error {
 	peerDir := GetPeerDir(username, peerServerAddress, peerUsername)
 	return WriteUint32ToFile(peerDir, "counter_out.txt", value)
