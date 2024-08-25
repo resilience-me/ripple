283e8dc74baa986e83586876a1cdfdd811fee754
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 08:36:20 +0200

    Update write.go

--- database/db_trustlines/write.go
+++ database/db_trustlines/write.go
@@ -17,24 +17,6 @@ func SetTrustlineIn(username, peerServerAddress, peerUsername string, value uint
 	return database.WriteUint32ToFile(trustlineDir, "trustline_in.txt", value)
 }
 
-// SetSyncCounter sets the sync_counter value.
-func SetSyncCounter(dg *types.Datagram, value uint32) error {
-	trustlineDir := database.GetTrustlineDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
-	return database.WriteUint32ToFile(trustlineDir, "sync_counter.txt", value)
-}
-
-// SetSyncIn sets the sync_in value.
-func SetSyncIn(dg *types.Datagram, value uint32) error {
-	trustlineDir := database.GetTrustlineDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
-	return database.WriteUint32ToFile(trustlineDir, "sync_in.txt", value)
-}
-
-// SetSyncOut sets the sync_out value.
-func SetSyncOut(dg *types.Datagram, value uint32) error {
-	trustlineDir := database.GetTrustlineDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
-	return database.WriteUint32ToFile(trustlineDir, "sync_out.txt", value)
-}
-
 // SetTimestamp sets the sync timestamp.
 func SetTimestamp(dg *types.Datagram, timestamp int64) error {
 	trustlineDir := database.GetTrustlineDir(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
