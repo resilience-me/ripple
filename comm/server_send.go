b7b1bf4dcb219a2aa99eeb2bc40b095a0ef665ce
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 14:46:57 +0200

    Update server_send.go

--- comm/server_send.go
+++ comm/server_send.go
@@ -2,10 +2,10 @@ package comm
 
 // Default Send with standard importance (5 retries)
 func Send(destinationAddr string, data []byte) error {
-	return SendWithResolvedAddress(destinationAddr, data, LowImportance)
+	return sendWithResolvedAddress(destinationAddr, data, LowImportance)
 }
 
 // Send with priority importance (12 retries)
 func SendPriority(destinationAddr string, data []byte) error {
-	return SendWithResolvedAddress(destinationAddr, data, HighImportance)
+	return sendWithResolvedAddress(destinationAddr, data, HighImportance)
 }
