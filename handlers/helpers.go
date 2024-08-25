3ce5268bda277b0c5d2d1895b074dd4257ee2ba4
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 09:26:02 +0200

    Update helpers.go

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -33,8 +33,8 @@ func PrepareDatagram(command byte, username, peerServerAddress, peerUsername str
     return datagram, nil
 }
 
-// PrepareDatagramResponse calls PrepareDatagram with fields from an incoming datagram
-func PrepareDatagramResponse(datagram *types.Datagram) (*types.Datagram, error) {
+// PrepareDatagramWithDatagram calls PrepareDatagram with fields from an incoming datagram
+func PrepareDatagramWithDatagram(datagram *types.Datagram) (*types.Datagram, error) {
     return PrepareDatagramWithoutCommand(datagram.Username, datagram.PeerServerAddress, datagram.PeerUsername)
 }
 
@@ -54,7 +54,7 @@ func PrepareAndSendDatagram(command byte, username, serverAddress, peerUsername 
     return nil
 }
 
-// PrepareAndSendDatagramWithDatagram calls PrepareAndSendDatagram with user identifiers taken from a Datagram
+// PrepareAndSendDatagramWithDatagram calls PrepareAndSendDatagram with with fields from an incoming datagram
 func PrepareAndSendDatagramWithDatagram(datagram *types.Datagram, command byte, arguments []byte) error {
     return PrepareAndSendDatagram(command, datagram.Username, datagram.PeerServerAddress, datagram.PeerUsername, arguments)
 }
