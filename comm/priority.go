b22e5b582c970fe029245ed1c9f0fcd3665244e7
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 14:47:26 +0200

    Update priority.go

--- comm/priority.go
+++ comm/priority.go
@@ -2,6 +2,6 @@ package comm
 
 // Retry levels based on importance
 const (
-	LowImportance    = 5  // 5 retries for standard messages
-	HighImportance   = 12 // 12 retries for priority messages
+	lowImportance    = 5  // 5 retries for standard messages
+	highImportance   = 12 // 12 retries for priority messages
 )
