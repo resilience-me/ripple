b0b1ce3c6b0db28576f4ddcbfe6b2dd1a4b11ac0
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 10:17:49 +0200

    Update syncTrustlineOut.go

--- handlers/trustlines/client_trustlines/syncTrustlineOut.go
+++ handlers/trustlines/client_trustlines/syncTrustlineOut.go
@@ -5,37 +5,37 @@ import (
 
     "ripple/commands"
     "ripple/comm"
-    "ripple/database/db_trustlines"
     "ripple/handlers"
     "ripple/types"
 )
 
 // SyncTrustlineOut handles the client request to sync the outbound trustline to the peer server.
 func SyncTrustlineOut(session types.Session) {
-    datagram := session.Datagram
+    // Instantiate a DatagramHelper using the NewDatagramHelper function
+    dh := handlers.NewDatagramHelper(session.Datagram)
 
-    // Read the trustline value
-    trustline, err := db_trustlines.GetTrustlineOutWithDatagram(datagram)
+    // Read the trustline value using DatagramHelper
+    trustline, err := dh.GetTrustlineOut()
     if err != nil {
-        log.Printf("Error getting trustline for user %s in SyncTrustlineOut: %v", datagram.Username, err)
+        log.Printf("Error getting trustline for user %s in SyncTrustlineOut: %v", dh.Username, err)
         comm.SendErrorResponse(session.Addr, "Failed to retrieve trustline.")
         return
     }
 
     arguments := types.Uint32ToBytes(trustline)
-    
-    // Prepare, sign, and send the datagram using the helper function from the handlers package
-    if err := handlers.PrepareAndSendDatagramWithDatagram(datagram, commands.ServerTrustlines_SetTrustline, arguments); err != nil {
-        log.Printf("Failed to prepare and send SetTrustline command from %s to peer %s at server %s: %v", datagram.Username, datagram.PeerUsername, datagram.PeerServerAddress, err)
+
+    // Prepare, sign, and send the datagram using the DatagramHelper
+    if err := dh.PrepareAndSendDatagram(commands.ServerTrustlines_SetTrustline, arguments); err != nil {
+        log.Printf("Failed to prepare and send SetTrustline command from %s to peer %s at server %s: %v", dh.Username, dh.PeerUsername, dh.PeerServerAddress, err)
         comm.SendErrorResponse(session.Addr, "Failed to send ServerTrustlines_SetTrustline command.")
         return
     }
 
     // Send success response to the client
     if err := comm.SendSuccessResponse(session.Addr, []byte("Outbound trustline sync request processed successfully.")); err != nil {
-        log.Printf("Failed to send success response in SyncTrustlineOut for user %s: %v", datagram.Username, err)
+        log.Printf("Failed to send success response in SyncTrustlineOut for user %s: %v", dh.Username, err)
         return
     }
 
-    log.Printf("SyncTrustline command processed successfully for user %s to peer %s.", datagram.Username, datagram.PeerUsername)
+    log.Printf("SyncTrustline command processed successfully for user %s to peer %s.", dh.Username, dh.PeerUsername)
 }
