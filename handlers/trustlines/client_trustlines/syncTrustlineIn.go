96a201fc8d68196684faa9e9c573160895a7c377
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 10:16:58 +0200

    Update syncTrustlineIn.go

--- handlers/trustlines/client_trustlines/syncTrustlineIn.go
+++ handlers/trustlines/client_trustlines/syncTrustlineIn.go
@@ -1,7 +1,6 @@
 package client_trustlines
 
 import (
-    "encoding/binary"
     "log"
 
     "ripple/commands"
@@ -12,20 +11,21 @@ import (
 
 // SyncTrustlineIn handles the client request to sync the inbound trustline from the peer server.
 func SyncTrustlineIn(session types.Session) {
-    datagram := session.Datagram
+    // Instantiate a DatagramHelper using the NewDatagramHelper function
+    dh := handlers.NewDatagramHelper(session.Datagram)
 
-    // Prepare, sign, and send the datagram using the helper function from the handlers package
-    if err := handlers.PrepareAndSendDatagramWithDatagram(datagram, commands.ServerTrustlines_GetTrustline, nil); err != nil {
-        log.Printf("Failed to prepare and send GetTrustline command from %s to peer %s at server %s: %v", datagram.Username, datagram.PeerUsername, datagram.PeerServerAddress, err)
+    // Prepare, sign, and send the datagram using the DatagramHelper
+    if err := dh.PrepareAndSendDatagram(commands.ServerTrustlines_GetTrustline, nil); err != nil {
+        log.Printf("Failed to prepare and send GetTrustline command from %s to peer %s at server %s: %v", dh.Username, dh.PeerUsername, dh.PeerServerAddress, err)
         comm.SendErrorResponse(session.Addr, "Failed to send GetTrustline command.")
         return
     }
 
     // Send success response to the client
     if err := comm.SendSuccessResponse(session.Addr, []byte("Trustline sync request sent successfully.")); err != nil {
-        log.Printf("Failed to send success response to user %s: %v", datagram.Username, err)
+        log.Printf("Failed to send success response to user %s: %v", dh.Username, err)
         return
     }
 
-    log.Printf("GetTrustline command sent successfully for user %s to peer %s.", datagram.Username, datagram.PeerUsername)
+    log.Printf("GetTrustline command sent successfully for user %s to peer %s.", dh.Username, dh.PeerUsername)
 }
