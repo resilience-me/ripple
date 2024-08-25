5de1945bf4248b357e2a5517e31a2df6dd065863
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 22:29:42 +0200

    Update setupTrustline.go

--- tests/setupTrustline.go
+++ tests/setupTrustline.go
@@ -2,18 +2,18 @@ package tests
 
 import (
     "log"
-    "ripple/database"
+    "ripple/database/db_trustlines"
 )
 
 // setupTrustline sets up the incoming and outgoing trustlines between two accounts.
 func setupTrustline(senderUsername, receiverUsername, serverAddress string, trustlineAmount uint32) error {
     // Set the incoming trustline for the sender from the receiver
-    if err := database.SetTrustlineIn(senderUsername, serverAddress, receiverUsername, trustlineAmount); err != nil {
+    if err := db_trustlines.SetTrustlineIn(senderUsername, serverAddress, receiverUsername, trustlineAmount); err != nil {
         return fmt.Errorf("failed to set trustlineIn for sender: %w", err)
     }
 
     // Set the outgoing trustline for the receiver to the sender
-    if err := database.SetTrustlineOut(receiverUsername, serverAddress, senderUsername, trustlineAmount); err != nil {
+    if err := db_trustlines.SetTrustlineOut(receiverUsername, serverAddress, senderUsername, trustlineAmount); err != nil {
         return fmt.Errorf("failed to set trustlineOut for receiver: %w", err)
     }
 
