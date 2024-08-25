0a7e80f776be40d1164bc9280343fbfcb185892e
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 10:15:14 +0200

    Update getTrustlineOut.go

--- handlers/trustlines/client_trustlines/getTrustlineOut.go
+++ handlers/trustlines/client_trustlines/getTrustlineOut.go
@@ -4,18 +4,19 @@ import (
     "log"
 
     "ripple/comm"
-    "ripple/database/db_trustlines"
+    "ripple/handlers"
     "ripple/types"
 )
 
 // GetTrustlineOut handles fetching the outbound trustline information
 func GetTrustlineOut(session types.Session) {
-    datagram := session.Datagram
+    // Instantiate a DatagramHelper using the NewDatagramHelper function
+    dh := handlers.NewDatagramHelper(session.Datagram)
 
-    // Fetch the outbound trustline
-    trustline, err := db_trustlines.GetTrustlineOutWithDatagram(datagram)
+    // Fetch the outbound trustline using DatagramHelper
+    trustline, err := dh.GetTrustlineOut()
     if err != nil {
-        log.Printf("Error reading outbound trustline for user %s: %v", datagram.Username, err)
+        log.Printf("Error reading outbound trustline for user %s: %v", dh.Username, err)
         comm.SendErrorResponse(session.Addr, "Error reading outbound trustline.")
         return
     }
@@ -25,9 +26,9 @@ func GetTrustlineOut(session types.Session) {
 
     // Send the success response back to the client
     if err := comm.SendSuccessResponse(session.Addr, responseData); err != nil {
-        log.Printf("Error sending success response to user %s: %v", datagram.Username, err)
+        log.Printf("Error sending success response to user %s: %v", dh.Username, err)
         return
     }
 
-    log.Printf("Outbound trustline sent successfully to user %s.", datagram.Username) // Log successful operation
+    log.Printf("Outbound trustline sent successfully to user %s.", dh.Username) // Log successful operation
 }
