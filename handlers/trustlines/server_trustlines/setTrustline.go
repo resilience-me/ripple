b1e2b99bb79f86a7feae587bdfd4cab423d91eae
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 10:19:52 +0200

    Update setTrustline.go

--- handlers/trustlines/server_trustlines/setTrustline.go
+++ handlers/trustlines/server_trustlines/setTrustline.go
@@ -4,26 +4,28 @@ import (
     "log"
     "time"
     "ripple/types"
+    "ripple/handlers"
     "ripple/database/db_trustlines"
 )
 
 // SetTrustline handles setting or updating a trustline from another server's perspective.
 func SetTrustline(session types.Session) {
-    datagram := session.Datagram
+    // Instantiate a DatagramHelper using the NewDatagramHelper function
+    dh := handlers.NewDatagramHelper(session.Datagram)
 
     // Retrieve the trustline amount from the Datagram
-    trustlineAmount := types.BytesToUint32(datagram.Arguments[:4])
+    trustlineAmount := types.BytesToUint32(dh.Arguments[:4])
 
-    // Update the trustline and timestamp
-    if err := db_trustlines.SetTrustlineInWithDatagram(datagram, trustlineAmount); err != nil {
-        log.Printf("Error writing trustline to file for user %s: %v", datagram.Username, err)
+    // Update the trustline and timestamp using DatagramHelper
+    if err := dh.SetTrustlineIn(trustlineAmount); err != nil {
+        log.Printf("Error writing trustline to file for user %s: %v", dh.Username, err)
         return
     }
 
-    if err := db_trustlines.SetTimestamp(datagram, time.Now().Unix()); err != nil {
-        log.Printf("Error writing timestamp to file for user %s: %v", datagram.Username, err)
+    if err := db_trustlines.SetTimestamp(dh.Datagram, time.Now().Unix()); err != nil {
+        log.Printf("Error writing timestamp to file for user %s: %v", dh.Username, err)
         return
     }
 
-    log.Printf("Trustline synchronization timestamp updated successfully for user %s.", datagram.Username)
+    log.Printf("Trustline synchronization timestamp updated successfully for user %s.", dh.Username)
 }
