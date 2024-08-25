706c2c6b23d32fef8dca3fbf32fb5a0a9347fe3a
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 14:47:13 +0200

    Update client_send.go

--- comm/client_send.go
+++ comm/client_send.go
@@ -8,7 +8,7 @@ import (
 // SendSuccessResponse sends a success message using the provided address with retry logic.
 func SendSuccessResponse(addr *net.UDPAddr, data []byte) error {
 	response := append([]byte{0}, data...) // Combine success indicator and message
-	if err := SendWithAddress(addr, response, HighImportance); err != nil {
+	if err := sendWithAddress(addr, response, highImportance); err != nil {
 		return fmt.Errorf("error sending success response: %w", err)
 	}
 	return nil
@@ -17,7 +17,7 @@ func SendSuccessResponse(addr *net.UDPAddr, data []byte) error {
 // SendErrorResponse sends an error message using the provided address with retry logic.
 func SendErrorResponse(addr *net.UDPAddr, message string) error {
 	response := append([]byte{1}, []byte(message)...) // Combine error indicator and message
-	if err := SendWithAddress(addr, response, HighImportance); err != nil {
+	if err := sendWithAddress(addr, response, highImportance); err != nil {
 		return fmt.Errorf("error sending error response: %w", err)
 	}
 	return nil
