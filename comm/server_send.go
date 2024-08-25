c6c421d4909285528d12b32bba45a104e18cfc69
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 11:43:52 +0200

    Update server_send.go

--- comm/server_send.go
+++ comm/server_send.go
@@ -1,33 +1,11 @@
 package comm
 
-import (
-    "fmt"
-    "ripple/types"
-    "ripple/auth"
-)
-
-// signAndSendDatagram creates a signed datagram and sends it over the network with custom priority
-func signAndSendDatagram(dg *types.Datagram, peerServerAddress string, maxRetries int) error {
-    // Create the signed datagram
-    serializedData, err := auth.SignDatagram(dg, peerServerAddress)
-    if err != nil {
-        return fmt.Errorf("failed to create signed datagram: %w", err)
-    }
-    
-    // Send the signed datagram over the network
-    if err := SendWithResolvedAddress(peerServerAddress, serializedData, maxRetries); err != nil {
-        return fmt.Errorf("failed to send datagram: %w", err)
-    }
-
-    return nil // Successfully signed and sent
+// Default Send with standard importance (5 retries)
+func Send(destinationAddr string, data []byte) error {
+	return SendWithResolvedAddress(destinationAddr, data, LowImportance)
 }
 
-// SignAndSendDatagram creates a signed datagram and sends it over the network with low priority.
-func SignAndSendDatagram(dg *types.Datagram, peerServerAddress string) error {
-    return signAndSendDatagram(dg, peerServerAddress, LowImportance)
-}
-
-// SignAndSendPriorityDatagram creates a signed datagram and sends it over the network with high priority.
-func SignAndSendPriorityDatagram(dg *types.Datagram, peerServerAddress string) error {
-    return signAndSendDatagram(dg, peerServerAddress, HighImportance)
+// Send with priority importance (12 retries)
+func SendPriority(destinationAddr string, data []byte) error {
+	return SendWithResolvedAddress(destinationAddr, data, HighImportance)
 }
