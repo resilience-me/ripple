4ec2cd9e958e6167bcc6524e6d5e2628dad705c8
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 10:13:34 +0200

    Update datagram_helper.go

--- handlers/datagram_helper.go
+++ handlers/datagram_helper.go
@@ -8,7 +8,7 @@ import (
 
 // DatagramHelper provides utility methods for working with a Datagram instance
 type DatagramHelper struct {
-    Datagram *types.Datagram
+    *types.Datagram // Embedding Datagram directly
 }
 
 // NewDatagramHelper creates a new DatagramHelper instance
@@ -16,32 +16,32 @@ func NewDatagramHelper(datagram *types.Datagram) DatagramHelper {
     return DatagramHelper{Datagram: datagram}
 }
 
-// GetTrustlineOut retrieves the outbound trustline using the wrapped Datagram
+// GetTrustlineOut retrieves the outbound trustline using the embedded Datagram
 func (dh DatagramHelper) GetTrustlineOut() (uint32, error) {
-    return db_trustlines.GetTrustlineOut(dh.Datagram.Username, dh.Datagram.PeerServerAddress, dh.Datagram.PeerUsername)
+    return db_trustlines.GetTrustlineOut(dh.Username, dh.PeerServerAddress, dh.PeerUsername)
 }
 
-// GetTrustlineIn retrieves the inbound trustline using the wrapped Datagram
+// GetTrustlineIn retrieves the inbound trustline using the embedded Datagram
 func (dh DatagramHelper) GetTrustlineIn() (uint32, error) {
-    return db_trustlines.GetTrustlineIn(dh.Datagram.Username, dh.Datagram.PeerServerAddress, dh.Datagram.PeerUsername)
+    return db_trustlines.GetTrustlineIn(dh.Username, dh.PeerServerAddress, dh.PeerUsername)
 }
 
-// SetTrustlineOut sets the outbound trustline amount using the wrapped Datagram
+// SetTrustlineOut sets the outbound trustline amount using the embedded Datagram
 func (dh DatagramHelper) SetTrustlineOut(value uint32) error {
-    return db_trustlines.SetTrustlineOut(dh.Datagram.Username, dh.Datagram.PeerServerAddress, dh.Datagram.PeerUsername, value)
+    return db_trustlines.SetTrustlineOut(dh.Username, dh.PeerServerAddress, dh.PeerUsername, value)
 }
 
-// SetTrustlineIn sets the inbound trustline amount using the wrapped Datagram
+// SetTrustlineIn sets the inbound trustline amount using the embedded Datagram
 func (dh DatagramHelper) SetTrustlineIn(value uint32) error {
-    return db_trustlines.SetTrustlineIn(dh.Datagram.Username, dh.Datagram.PeerServerAddress, dh.Datagram.PeerUsername, value)
+    return db_trustlines.SetTrustlineIn(dh.Username, dh.PeerServerAddress, dh.PeerUsername, value)
 }
 
-// PrepareDatagram prepares a new datagram using fields from the wrapped Datagram
+// PrepareDatagram prepares a new datagram using fields from the embedded Datagram
 func (dh DatagramHelper) PrepareDatagram() (*types.Datagram, error) {
-    return PrepareDatagramWithoutCommand(dh.Datagram.Username, dh.Datagram.PeerServerAddress, dh.Datagram.PeerUsername)
+    return PrepareDatagramWithoutCommand(dh.Username, dh.PeerServerAddress, dh.PeerUsername)
 }
 
-// PrepareAndSendDatagram prepares and sends a new datagram using fields from the wrapped Datagram
+// PrepareAndSendDatagram prepares and sends a new datagram using fields from the embedded Datagram
 func (dh DatagramHelper) PrepareAndSendDatagram(command byte, arguments []byte) error {
-    return PrepareAndSendDatagram(command, dh.Datagram.Username, dh.Datagram.PeerServerAddress, dh.Datagram.PeerUsername, arguments)
+    return PrepareAndSendDatagram(command, dh.Username, dh.PeerServerAddress, dh.PeerUsername, arguments)
 }
