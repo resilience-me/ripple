b002004dd5fb64eed55b8e9b210b9549c8987f2e
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 14:58:20 +0200

    Update validate_datagram.go

--- auth/validate_datagram.go
+++ auth/validate_datagram.go
@@ -25,38 +25,6 @@ func ValidatePeerExists(dg *types.Datagram) (string, error) {
 	return "", nil // No error, directories exist
 }
 
-// validateAndIncrementClientCounter checks if the datagram's counter is valid by comparing it to the last known counter for client connections.
-// If valid, it sets the counter to the value in the datagram to prevent replay attacks.
-func validateAndIncrementClientCounter(datagram *types.Datagram) error {
-	prevCounter, err := database.GetCounter(datagram.Username)
-	if err != nil {
-		return fmt.Errorf("error retrieving counter: %v", err)
-	}
-	if datagram.Counter <= prevCounter {
-		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen counter %d", datagram.Counter, prevCounter)
-	}
-	if err := database.SetCounter(datagram.Username, datagram.Counter); err != nil {
-		return fmt.Errorf("failed to set counter: %v", err)
-	}
-	return nil
-}
-
-// validateAndIncrementServerCounter checks if the datagram's counter is valid by comparing it to the last known counter for server connections.
-// If valid, it sets the counter to the value in the datagram to prevent replay attacks.
-func validateAndIncrementServerCounter(datagram *types.Datagram) error {
-	prevCounter, err := database.GetCounterIn(datagram.Username, datagram.PeerServerAddress, datagram.PeerUsername)
-	if err != nil {
-		return fmt.Errorf("error retrieving in-counter: %v", err)
-	}
-	if datagram.Counter <= prevCounter {
-		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen in-counter %d", datagram.Counter, prevCounter)
-	}
-	if err := database.SetCounterIn(datagram.Username, datagram.PeerServerAddress, datagram.PeerUsername, datagram.Counter); err != nil {
-		return fmt.Errorf("failed to set in-counter: %v", err)
-	}
-	return nil
-}
-
 // validateClientDatagram validates the client datagram and checks the counter
 func validateClientDatagram(buf []byte, dg *types.Datagram) error {
 	secretKey, err := auth.loadClientSecretKey(dg)
