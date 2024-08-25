48cca85eeb5ac42252e8d6d8190d298964368b04
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 11:58:04 +0200

    Update counters.go

--- auth/counters.go
+++ auth/counters.go
@@ -2,21 +2,21 @@ package auth
 
 import (
 	"fmt"
-	"ripple/types"
 	"ripple/database"
+	"ripple/types"
 )
 
 // validateAndIncrementClientCounter checks if the datagram's counter is valid by comparing it to the last known counter for client connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
 func validateAndIncrementClientCounter(datagram *types.Datagram) error {
-	prevCounter, err := database.GetCounter(datagram)
+	prevCounter, err := database.GetCounter(datagram.Username)
 	if err != nil {
 		return fmt.Errorf("error retrieving counter: %v", err)
 	}
 	if datagram.Counter <= prevCounter {
 		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen counter %d", datagram.Counter, prevCounter)
 	}
-	if err := database.SetCounter(datagram); err != nil {
+	if err := database.SetCounter(datagram.Username, datagram.Counter); err != nil {
 		return fmt.Errorf("failed to set counter: %v", err)
 	}
 	return nil
@@ -25,14 +25,14 @@ func validateAndIncrementClientCounter(datagram *types.Datagram) error {
 // validateAndIncrementServerCounter checks if the datagram's counter is valid by comparing it to the last known counter for server connections.
 // If valid, it sets the counter to the value in the datagram to prevent replay attacks.
 func validateAndIncrementServerCounter(datagram *types.Datagram) error {
-	prevCounter, err := database.GetCounterIn(datagram)
+	prevCounter, err := database.GetCounterIn(datagram.Username, datagram.PeerServerAddress, datagram.PeerUsername)
 	if err != nil {
 		return fmt.Errorf("error retrieving in-counter: %v", err)
 	}
 	if datagram.Counter <= prevCounter {
 		return fmt.Errorf("replay detected or old datagram: Counter %d is not greater than the last seen in-counter %d", datagram.Counter, prevCounter)
 	}
-	if err := database.SetCounterIn(datagram); err != nil {
+	if err := database.SetCounterIn(datagram.Username, datagram.PeerServerAddress, datagram.PeerUsername, datagram.Counter); err != nil {
 		return fmt.Errorf("failed to set in-counter: %v", err)
 	}
 	return nil
@@ -48,7 +48,7 @@ func GetAndIncrementCounterOut(username, peerServerAddress, peerUsername string)
     }
 
     // Increment the counter and update it in the database within the same function call.
-    if err := database.SetCounterOut(username, peerServerAddress, peerUsername, counterOut + 1); err != nil {
+    if err := database.SetCounterOut(username, peerServerAddress, peerUsername, counterOut+1); err != nil {
         return 0, err  // Return error if unable to update the counter.
     }
 
