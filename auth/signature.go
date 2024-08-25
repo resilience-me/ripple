cbd7e40973d203547511806ad8a767339b66b8a0
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 16:51:08 +0200

    Update signature.go

--- auth/signature.go
+++ auth/signature.go
@@ -21,27 +21,31 @@ func loadServerSecretKey(dg *types.Datagram) ([]byte, error) {
 
 // GenerateSignature generates a SHA-256 hash for the given datagram using the provided key.
 func GenerateSignature(data []byte, secretKey []byte) []byte {
-    // Remove the signature from the datagram
-    dataWithoutSignature := data[:len(data)-32]
+    // Create a temporary slice to hold the concatenated data (buf without the last 32 bytes) and the secret key.
+    preimage := make([]byte, len(data)) // Allocate slice with the length of buf
 
-    // Concatenate data and secret
-    preimage := append(dataWithoutSignature, secretKey...)
+    // Copy the data (excluding the last 32 bytes) into the preimage.
+    copy(preimage, buf[:len(data)-32])
 
-    // Compute the SHA-256 hash
+    // Append the secret key to the preimage.
+    copy(preimage[len(data)-32:], secretKey)
+
+    // Compute the SHA-256 hash of the preimage.
     hash := sha256.Sum256(preimage)
 
-    // Return the hash as a byte slice
+    // Return the hash as a byte slice.
     return hash[:]
 }
 
+
 // verifySignature checks the integrity of the received buffer
-func verifySignature(data []byte, key []byte) bool {
+func verifySignature(buf []byte, key []byte) bool {
     // The signature is the last 32 bytes of the buffer
     signature := make([]byte, 32)
-    copy(signature, data[len(data)-32:])
+    copy(signature, buf[len(buf)-32:])
 
     // Generate the expected signature using the GenerateSignature method
-    expectedSignature := GenerateSignature(data, key)
+    expectedSignature := GenerateSignature(buf, key)
 
     // Compare the signature with the expected signature directly using bytes.Equal
     return bytes.Equal(signature, expectedSignature)
