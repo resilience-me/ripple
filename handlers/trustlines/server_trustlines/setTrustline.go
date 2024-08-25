31773506f2abce211d5fe7676e47752ca5fdf2d2
resilience-me <emailsareobsolete@gmail.com>
2024-08-25 10:23:46 +0200

    Update setTrustline.go

--- handlers/trustlines/server_trustlines/setTrustline.go
+++ handlers/trustlines/server_trustlines/setTrustline.go
@@ -2,10 +2,8 @@ package server_trustlines
 
 import (
     "log"
-    "time"
     "ripple/types"
     "ripple/handlers"
-    "ripple/database/db_trustlines"
 )
 
 // SetTrustline handles setting or updating a trustline from another server's perspective.
@@ -16,13 +14,14 @@ func SetTrustline(session types.Session) {
     // Retrieve the trustline amount from the Datagram
     trustlineAmount := types.BytesToUint32(dh.Arguments[:4])
 
-    // Update the trustline and timestamp using DatagramHelper
+    // Update the trustline using DatagramHelper
     if err := dh.SetTrustlineIn(trustlineAmount); err != nil {
         log.Printf("Error writing trustline to file for user %s: %v", dh.Username, err)
         return
     }
 
-    if err := db_trustlines.SetTimestamp(dh.Datagram, time.Now().Unix()); err != nil {
+    // Update the timestamp using DatagramHelper
+    if err := dh.SetTimestamp(); err != nil {
         log.Printf("Error writing timestamp to file for user %s: %v", dh.Username, err)
         return
     }
