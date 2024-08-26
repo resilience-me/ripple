992477636479ca1c8324b682804e2d69e40eebe4
resilience-me <emailsareobsolete@gmail.com>
2024-08-26 14:53:31 +0200

    Update commands.go

--- commands/commands.go
+++ commands/commands.go
@@ -17,3 +17,23 @@ const (
     ServerPayments_FindPathIn          = 130
     ServerPayments_PathRecurse         = 131
 )
+
+// commandNames maps command bytes to their names
+var commandNames = [256]string{
+    0:   "ClientTrustlines_SetTrustline",
+    1:   "ClientTrustlines_SyncTrustlineIn",
+    2:   "ClientTrustlines_SyncTrustlineOut",
+    3:   "ClientTrustlines_GetTrustlineIn",
+    4:   "ClientTrustlines_GetTrustlineOut",
+    5:   "ClientPayments_NewPaymentOut",
+    6:   "ClientPayments_NewPaymentIn",
+    7:   "ClientPayments_GetPayment",
+    8:   "ClientPayments_StartPayment",
+
+    127: "ServerTrustlines_SetTrustline",
+    128: "ServerTrustlines_GetTrustline",
+    129: "ServerPayments_FindPathOut",
+    130: "ServerPayments_FindPathIn",
+    131: "ServerPayments_PathRecurse",
+    // Other indices are empty strings by default
+}
