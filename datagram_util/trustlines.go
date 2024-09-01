0db745dc07633efbb9a5d8750c07dcec938769a0
resilience-me <emailsareobsolete@gmail.com>
2024-09-01 20:58:50 +0200

    Update trustlines.go

--- datagram_util/trustlines.go
+++ datagram_util/trustlines.go
@@ -1,25 +1,32 @@
-// GetTrustlineOut retrieves the outbound trustline using the embedded Datagram
-func (dh DatagramHelper) GetTrustlineOut() (uint32, error) {
-    return db_trustlines.GetTrustlineOut(dh.Username, dh.PeerServerAddress, dh.PeerUsername)
+package datagram_util
+
+import (
+    "ripple/db_trustlines" // Import the db_trustlines package
+    "time"
+)
+
+// GetTrustlineOut retrieves the outbound trustline using the embedded Datagram.
+func (dg *Datagram) GetTrustlineOut() (uint32, error) {
+    return db_trustlines.GetTrustlineOut(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
 }
 
-// GetTrustlineIn retrieves the inbound trustline using the embedded Datagram
-func (dh DatagramHelper) GetTrustlineIn() (uint32, error) {
-    return db_trustlines.GetTrustlineIn(dh.Username, dh.PeerServerAddress, dh.PeerUsername)
+// GetTrustlineIn retrieves the inbound trustline using the embedded Datagram.
+func (dg *Datagram) GetTrustlineIn() (uint32, error) {
+    return db_trustlines.GetTrustlineIn(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
 }
 
-// SetTrustlineOut sets the outbound trustline amount using the embedded Datagram
-func (dh DatagramHelper) SetTrustlineOut(value uint32) error {
-    return db_trustlines.SetTrustlineOut(dh.Username, dh.PeerServerAddress, dh.PeerUsername, value)
+// SetTrustlineOut sets the outbound trustline amount using the embedded Datagram.
+func (dg *Datagram) SetTrustlineOut(value uint32) error {
+    return db_trustlines.SetTrustlineOut(dg.Username, dg.PeerServerAddress, dg.PeerUsername, value)
 }
 
-// SetTrustlineIn sets the inbound trustline amount using the embedded Datagram
-func (dh DatagramHelper) SetTrustlineIn(value uint32) error {
-    return db_trustlines.SetTrustlineIn(dh.Username, dh.PeerServerAddress, dh.PeerUsername, value)
+// SetTrustlineIn sets the inbound trustline amount using the embedded Datagram.
+func (dg *Datagram) SetTrustlineIn(value uint32) error {
+    return db_trustlines.SetTrustlineIn(dg.Username, dg.PeerServerAddress, dg.PeerUsername, value)
 }
 
-// SetTimestamp sets the current Unix timestamp for the Datagram
-func (dh DatagramHelper) SetTimestamp() error {
+// SetTimestamp sets the current Unix timestamp for the Datagram.
+func (dg *Datagram) SetTimestamp() error {
     now := time.Now().Unix()
-    return db_trustlines.SetTimestamp(dh.Username, dh.PeerServerAddress, dh.PeerUsername, now)
+    return db_trustlines.SetTimestamp(dg.Username, dg.PeerServerAddress, dg.PeerUsername, now)
 }
