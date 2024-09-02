decd71c02a7780ed37b17c6f3174d5575cfa9ee2
resilience-me <emailsareobsolete@gmail.com>
2024-09-02 15:01:47 +0200

    Update counter_io.go

--- datagram_util/counter_io.go
+++ datagram_util/counter_io.go
@@ -3,31 +3,31 @@ package datagram_util
 import "ripple/database"
 
 // GetCounter retrieves the client's counter.
-func (dh DatagramHelper) GetCounter() (uint32, error) {
-    return database.GetCounter(dh.Username)
+func (dg *Datagram) GetCounter() (uint32, error) {
+    return database.GetCounter(dg.Username)
 }
 
 // SetCounter sets the client's counter.
-func (dh DatagramHelper) SetCounter() error {
-    return database.SetCounter(dh.Username, dh.Counter)
+func (dg *Datagram) SetCounter() error {
+    return database.SetCounter(dg.Username, dg.Counter)
 }
 
 // GetCounterIn retrieves the incoming counter for a server connection.
-func (dh DatagramHelper) GetCounterIn() (uint32, error) {
-    return database.GetCounterIn(dh.Username, dh.PeerServerAddress, dh.PeerUsername)
+func (dg *Datagram) GetCounterIn() (uint32, error) {
+    return database.GetCounterIn(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
 }
 
 // SetCounterIn sets the incoming counter for a server connection.
-func (dh DatagramHelper) SetCounterIn() error {
-    return database.SetCounterIn(dh.Username, dh.PeerServerAddress, dh.PeerUsername, dh.Counter)
+func (dg *Datagram) SetCounterIn() error {
+    return database.SetCounterIn(dg.Username, dg.PeerServerAddress, dg.PeerUsername, dg.Counter)
 }
 
 // GetCounterOut retrieves the outgoing counter for a server connection.
-func (dh DatagramHelper) GetCounterOut() (uint32, error) {
-    return database.GetCounterOut(dh.Username, dh.PeerServerAddress, dh.PeerUsername)
+func (dg *Datagram) GetCounterOut() (uint32, error) {
+    return database.GetCounterOut(dg.Username, dg.PeerServerAddress, dg.PeerUsername)
 }
 
 // SetCounterOut sets the outgoing counter for a server connection.
-func (dh DatagramHelper) SetCounterOut(value uint32) error {
-    return database.SetCounterOut(dh.Username, dh.PeerServerAddress, dh.PeerUsername, value)
+func (dg *Datagram) SetCounterOut(value uint32) error {
+    return database.SetCounterOut(dg.Username, dg.PeerServerAddress, dg.PeerUsername, value)
 }
